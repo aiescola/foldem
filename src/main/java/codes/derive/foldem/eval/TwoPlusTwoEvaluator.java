e3eea51703b540c6396063ce7c4721d50fc7b563
user <email@email.com>
2018-10-16 11:05:37 +1300

    Add an implementation of the two-plus-two evaluator

--- src/main/java/codes/derive/foldem/eval/TwoPlusTwoEvaluator.java
+++ src/main/java/codes/derive/foldem/eval/TwoPlusTwoEvaluator.java
@@ -1,109 +1,114 @@
+/*
+ * This file is part of Fold'em, a Java library for Texas Hold 'em Poker.
+ *
+ * Fold'em is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fold'em is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fold'em.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package codes.derive.foldem.eval;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
 
 import codes.derive.foldem.Card;
 import codes.derive.foldem.Hand;
-import codes.derive.foldem.Poker;
 import codes.derive.foldem.board.Board;
 
+/**
+ * An implementation of {@link Evaluator} that uses the Two-Plus-Two algorithm.
+ */
 public class TwoPlusTwoEvaluator implements Evaluator {
-
+	
 	/* The number of rankings in the lookup table */
 	private static final int NUM_RANKINGS = 32487834;
 	
 	/* The card values as they appear in the lookup table */
 	private static final int[] CARD_VALUES = {
-			
+		52, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, /* Spades */
+		49, 1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45,  /* Clubs */
+		51, 3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, /* Hearts */
+		50, 2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46  /* Diamonds */
 	};
 	
-	private int[] rankTable;
+	/* The default path the evaluator will look for when loading the rank table data */
+	private static final Path DEFAULT_PATH = Paths.get("rankings.dat");
 	
-	/*
+	/* The rank table */
+	private final int[] rankTable = new int[NUM_RANKINGS];
+	
+	/**
+	 * Constructs a new TwoPlusTWoEvaluator using the lookup table data provided at
+	 * the specified path.
 	 * 
-	TWO_OF_CLUBS(TWO, CLUBS, 1),
-	THREE_OF_CLUBS(THREE, CLUBS, 5),
-	FOUR_OF_CLUBS(FOUR, CLUBS, 9),
-	FIVE_OF_CLUBS(FIVE, CLUBS, 13),
-	SIX_OF_CLUBS(SIX, CLUBS, 17),
-	SEVEN_OF_CLUBS(SEVEN, CLUBS, 21),
-	EIGHT_OF_CLUBS(EIGHT, CLUBS, 25),
-	NINE_OF_CLUBS(NINE, CLUBS, 29),
-	TEN_OF_CLUBS(TEN, CLUBS, 33),
-	JACK_OF_CLUBS(JACK, CLUBS, 37),
-	QUEEN_OF_CLUBS(QUEEN, CLUBS, 41),
-	KING_OF_CLUBS(KING, CLUBS, 45),
-	ACE_OF_CLUBS(ACE, CLUBS, 49),
-
-	TWO_OF_DIAMONDS(TWO, DIAMONDS, 2),
-	THREE_OF_DIAMONDS(THREE, DIAMONDS, 6),
-	FOUR_OF_DIAMONDS(FOUR, DIAMONDS, 10),
-	FIVE_OF_DIAMONDS(FIVE, DIAMONDS, 14),
-	SIX_OF_DIAMONDS(SIX, DIAMONDS, 18),
-	SEVEN_OF_DIAMONDS(SEVEN, DIAMONDS, 22),
-	EIGHT_OF_DIAMONDS(EIGHT, DIAMONDS, 26),
-	NINE_OF_DIAMONDS(NINE, DIAMONDS, 30),
-	TEN_OF_DIAMONDS(TEN, DIAMONDS, 34),
-	JACK_OF_DIAMONDS(JACK, DIAMONDS, 38),
-	QUEEN_OF_DIAMONDS(QUEEN, DIAMONDS, 42),
-	KING_OF_DIAMONDS(KING, DIAMONDS, 46),
-	ACE_OF_DIAMONDS(ACE, DIAMONDS, 50),
-
-	TWO_OF_HEARTS(TWO, HEARTS, 3),
-	THREE_OF_HEARTS(THREE, HEARTS, 7),
-	FOUR_OF_HEARTS(FOUR, HEARTS, 11),
-	FIVE_OF_HEARTS(FIVE, HEARTS, 15),
-	SIX_OF_HEARTS(SIX, HEARTS, 19),
-	SEVEN_OF_HEARTS(SEVEN, HEARTS, 23),
-	EIGHT_OF_HEARTS(EIGHT, HEARTS, 27),
-	NINE_OF_HEARTS(NINE, HEARTS, 31),
-	TEN_OF_HEARTS(TEN, HEARTS, 35),
-	JACK_OF_HEARTS(JACK, HEARTS, 39),
-	QUEEN_OF_HEARTS(QUEEN, HEARTS, 43),
-	KING_OF_HEARTS(KING, HEARTS, 47),
-	ACE_OF_HEARTS(ACE, HEARTS, 51),
-
-	TWO_OF_SPADES(TWO, SPADES, 4),
-	THREE_OF_SPADES(THREE, SPADES, 8),
-	FOUR_OF_SPADES(FOUR, SPADES, 12),
-	FIVE_OF_SPADES(FIVE, SPADES, 16),
-	SIX_OF_SPADES(SIX, SPADES, 20),
-	SEVEN_OF_SPADES(SEVEN, SPADES,24),
-	EIGHT_OF_SPADES(EIGHT, SPADES,28),
-	NINE_OF_SPADES(NINE, SPADES, 32),
-	TEN_OF_SPADES(TEN, SPADES, 36),
-	JACK_OF_SPADES(JACK, SPADES, 40),
-	QUEEN_OF_SPADES(QUEEN, SPADES, 44),
-	KING_OF_SPADES(KING, SPADES, 48),
-ACE_OF_SPADES(ACE, SPADES, 52);
+	 * @param tableDataFile
+	 *            A path to the table data file.
+	 * @throws IOException
+	 *             Thrown if the table data file could not be opened or read.
 	 */
-	
-	public TwoPlusTwoEvaluator(Path tableDataFile) {
-		
+	public TwoPlusTwoEvaluator(Path tableDataFile) throws IOException {
+		loadRankTable(tableDataFile, rankTable);
 	}
 
+	/**
+	 * Constructs a new TwoPlusTwoEvaluator using the default lookup table data
+	 * path.
+	 * 
+	 * @throws IOException
+	 *             Thrown if the table data file could not be opened or read.
+	 */
+	public TwoPlusTwoEvaluator() throws IOException {
+		this(DEFAULT_PATH);
+	}
+
+	/*
+	 * Loads the ranking lookup table for the Two-Plus-Two evaluator. 
+	 * @param path
+	 * 		The path to the LT file.
+	 * @param ranks
+	 * 		The array to write the loaded table into
+	 * @throws IOException
+	 * 		Thrown if the table could not be read.
+	 */
+	private static void loadRankTable(Path path, int[] ranks) throws IOException {
+		FileChannel channel = FileChannel.open(path);
+		
+		ByteBuffer buf = ByteBuffer.allocateDirect(NUM_RANKINGS * 4);
+		while (channel.read(buf) != -1);
+		buf.order(ByteOrder.LITTLE_ENDIAN);
+		
+		IntStream.range(0, NUM_RANKINGS).parallel().forEach((i) -> ranks[i] = buf.getInt(i * 4));
+		channel.close();
+	}
+	
 	@Override
-	public int rank(Hand h, Board b) {
+	public int rank(Hand h, Board b) { // TODO naming
 		int p = 53;
-		for (Card c : h.cards()) {
-			p = rankTable[p + CARD_VALUES[c.getValue()]];
-		}
 		for (Card c : b.cards()) {
-			p = rankTable[p + CARD_VALUES[c.getValue()]];
+			p = rankTable[p + CARD_VALUES[c.getId()]];
+		}
+		for (Card c : h.cards()) {
+			p = rankTable[p + CARD_VALUES[c.getId()]];
 		}
 		return p;
 	}
 	
 	@Override
 	public HandValue value(Hand hand, Board board) {
-		return null; // (shrug) TODO
+		return HandValue.values()[rank(hand, board) >> 12];
 	}
 
-	public static void main(String... args) {
-		Poker.cards().forEach(c -> {
-			System.out.println(c.getId() + " " + c.toString());
-		});
-	}
-	
-	
 }
---
+++ src/test/java/codes/derive/foldem/eval/TestEvaluatorComparison.java
@@ -0,0 +1,32 @@
+package codes.derive.foldem.eval;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import org.junit.Test;
+
+import codes.derive.foldem.Deck;
+import codes.derive.foldem.Hand;
+import codes.derive.foldem.Poker;
+import codes.derive.foldem.board.Board;
+import codes.derive.foldem.board.Boards;
+
+public class TestEvaluatorComparison {
+
+	@Test
+	public void compareTwoPlusTwoWithDefault() throws IOException {
+		compareEvaluators(new DefaultEvaluator(), new TwoPlusTwoEvaluator());
+	}
+	
+	private void compareEvaluators(Evaluator e1, Evaluator e2) {
+		for (int i  = 0; i < 100000 /* Arbitrary */; i++) {
+			Deck deck = Poker.shuffledDeck();
+			Board board = Boards.random(deck);
+			Hand a = Poker.hand(deck), b = Poker.hand(deck);
+			assertEquals(e1.rank(a, board) > e1.rank(b, board), e2.rank(a, board) < e2.rank(b, board));
+			assertEquals(e1.value(a, board), e2.value(a, board));
+			assertEquals(e1.value(b, board), e2.value(b, board));
+		}
+	}
+
+}
